df7c80ca2459a11e9f6991cf70545074335756b4
GammaZor <GammaRSA@gmail.com>
2015-10-22 14:10:24 +0200

    imagebrancecomment

--- MobileAPp/MobileAPp/Form1.cs
+++ MobileAPp/MobileAPp/Form1.cs
@@ -16,5 +16,10 @@ namespace MobileAPp
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //This is comment
+        }
     }
 }
